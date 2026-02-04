959c2887411b03ee8ecbe9cf5acc6052530730ff
Kim Josell <145394112+kimjosell@users.noreply.github.com>
2026-02-04 14:57:36 -0700

    Last changes

--- angular-project/find-my-room-app/src/pages/map-page/map-page.ts
+++ angular-project/find-my-room-app/src/pages/map-page/map-page.ts
@@ -7,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrl: './map-page.css',
 })
 export class MapPage {
+  items = [
+    { direction: 'Turn right at the next intersection', imgSrc: 'assets/turn-right.png' },
+    { direction: 'Walk straight for 200 meters', imgSrc: 'assets/walk-straight.png' },
+    { direction: 'Your destination will be on the left', imgSrc: 'assets/destination-left.png' }
+  ];
 
+  getIcon(direction:string):string 
+  {
+    if(direction.includes('right')) return 'turn_right';
+    if(direction.includes('left')) return 'turn_left';
+    if(direction.includes("straight")) return 'straight';
+    return "location_on";
+  }
 }
